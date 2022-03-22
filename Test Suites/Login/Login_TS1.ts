aa84e478716bd73974743c7455937ff67ab12821
aneeshjo <aneeshjose012@gmail.com>
2022-03-22 09:38:38 +0530

    First Commit

---
+++ Test Suites/Login/Login_TS1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>79508a26-df39-4b26-9ec7-ab7226ad0921</testSuiteGuid>
+   <testCaseLink>
+      <guid>bea6b7d3-3fe9-47be-a635-089ad09d1d38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OrangeHRM/LoginFunction_TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
